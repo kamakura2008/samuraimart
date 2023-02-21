340e2c75a33df13d0fb81e2923ef21664c7dbaad
shin-ichi kamakura <s-kamakura@imacocolab.com>
2023-02-21 04:07:19 +0000

    05 create login system

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
-  # get 'products/index'
-  # get 'products/show'
-  # get 'products/new'
-  # get 'products/create'
-  # get 'products/edit'
-  # get 'products/update'
-  # get 'products/destroy'
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations',
+    :unlocks => 'users/unlocks',
+  }
+  
+  devise_scope :user do
+    root :to => "users/sessions#new"
+    get "signup", :to => "users/registrations#new"
+    get "verify", :to => "users/registrations#verify"
+    get "login", :to => "users/sessions#new"
+    delete "logout", :to => "users/sessions#destroy"
+  end
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
