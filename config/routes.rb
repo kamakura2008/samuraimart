8a9e5550f63c399b1cd5e35e6cf6b207601d4b23
shin-ichi kamakura <s-kamakura@imacocolab.com>
2023-02-27 09:19:09 +0000

    11 favorite_function

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
   end
   # resources :products
   resources :products do
+    member do
+      get :favorite
+    end
     resources :reviews, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
