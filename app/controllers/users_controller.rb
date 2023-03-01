e2a053ba0b286647e64bd8f115431fef6decef67
shin-ichi kamakura <s-kamakura@imacocolab.com>
2023-03-01 09:55:38 +0000

    file_share

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_action :set_user
+  
+  def edit
+    # @user = current_user
+  end
+
+  def update
+    # @user = current_user
+    @user.update_without_password(user_params)
+    redirect_to mypage_users_url
+  end
+
+  def mypage
+    # @user = current_user
+  end
+  
+  def edit_address
+    # @user = current_user
+  end
+  
+  private
+  def set_user
+    @user = current_user
+  end
+  
+  def user_params
+    params.permit(:name, :email, :address, :phone, :password, :password_confirmation)
+  end
+end
