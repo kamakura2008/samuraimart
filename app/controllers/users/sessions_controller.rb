27fdd90190a3d2f79f498276185f880bc31bd140
shin-ichi kamakura <s-kamakura@imacocolab.com>
2023-03-02 02:19:17 +0000

    devise_fixed

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,28 +9,28 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  def create
+ def create
     super
-  end
+ end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
   
-  def after_sign_in_path_for(user)
+ def after_sign_in_path_for(user)
     products_path
-  end
-  
-  def after_sign_out_path_for(user)
+ end
+ 
+ def after_sign_out_path_for(user)
     root_path
-  end
+ end
 
-  protected
+ protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_in_params
+ # If you have extra params to permit, append them to the sanitizer.
+ def configure_sign_in_params
     devise_parameter_sanitizer.permit(
-      :sign_in, keys: [:name, :postal_code, :address, :phone, :email, :password, :password_confirmation])
-  end
+      :sign_in, keys: [ :name, :postal_code, :address, :phone, :email, :password, :password_confirmation ])
+ end
 end
