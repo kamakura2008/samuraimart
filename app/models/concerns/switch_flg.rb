69b5af8a20807434ad207bb0c7a44d44ab88a0a4
shin-ichi kamakura <s-kamakura@imacocolab.com>
2023-03-11 13:04:35 +0000

    admin_display7_before_roll_back

---
+++ app/models/concerns/switch_flg.rb
@@ -0,0 +1,13 @@
+# module SwitchFlg
+    
+# def switch_flg(column_of_obj)
+#     column_of_obj ? false : true
+# end
+# end
+
+module SwitchFlg
+
+  def switch_flg(column_of_obj)
+    column_of_obj ? false : true
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   end
   
   namespace :dashboard do
+    resources :users, only: [:index, :destroy]
     resources :major_categories, except: [:new]
     resources :categories, except: [:new]
     resources :products, except: [:show]
