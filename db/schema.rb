9872ecff245dfb30a74fdc72077f8c0142b12228
shin-ichi kamakura <s-kamakura@imacocolab.com>
2023-03-12 11:43:47 +0000

    admin_display7_after_migrate_reset

--- db/schema.rb
+++ db/schema.rb
@@ -129,7 +129,7 @@ ActiveRecord::Schema.define(version: 2023_03_11_123630) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "deleted_flag", default: false, null: false
+    t.boolean "deleted_flg", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
