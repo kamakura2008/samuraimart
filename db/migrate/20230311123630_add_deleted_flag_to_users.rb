3e9a8b2fde6a062e55d27fdef4a060d686ecbfa4
shin-ichi kamakura <s-kamakura@imacocolab.com>
2023-03-12 04:30:56 +0000

    admin_display7

--- db/migrate/20230311123630_add_deleted_flag_to_users.rb
+++ db/migrate/20230311123630_add_deleted_flag_to_users.rb
@@ -1,5 +1,6 @@
 class AddDeletedFlagToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :deleted_flag, :boolean, default: false, null: false
+    # rename_column :users, :deleted_flag, :deleted_flg
   end
 end
